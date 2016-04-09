c4d526c4fa844f0fcc5a786728fcbcabe311787a
Suhas Amireddy <suhasamireddy2@gmail.com>
2016-04-09 11:10:51 -0400

    Android Studio Project file

---
+++ TurtleMap/app/src/main/java/com/example/suhasamireddy/turtlemap/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.suhasamireddy.turtlemap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void startMapActivity(View view){
+        Intent intent = new Intent(this,MapsActivity.class);//Will have some parameters to start a new activity
+        startActivity(intent);
+    }
+}
