6d8436fe8bbb7f99fcfb0eacd832449135048738
Suhas Amireddy <suhasamireddy2@gmail.com>
2016-04-09 11:36:26 -0400

    Updated files

--- TurtleMap/app/src/main/java/com/example/suhasamireddy/turtlemap/MapsActivity.java
+++ TurtleMap/app/src/main/java/com/example/suhasamireddy/turtlemap/MapsActivity.java
@@ -38,8 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng mckeldin = new LatLng(38.985882, -76.944845);
+        mMap.addMarker(new MarkerOptions().position(mckeldin).title("Marker in College Park"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mckeldin));
     }
 }
